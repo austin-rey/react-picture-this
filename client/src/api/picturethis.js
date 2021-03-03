b430e3fd02f122a6e9baf156b0cc274bb22d0436
austin-rey <austinrey95@gmail.com>
2021-03-03 15:00:39 -0600

    Updated production endpoint for api requests

--- client/src/api/picturethis.js
+++ client/src/api/picturethis.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://localhost:5000/api/v1/`,
+  baseURL: `/api/v1/`,
   timeout: 600000,
 });
-
-// export default axios.create({
-//   baseURL: `/api/v1/`,
-//   timeout: 600000,
-// });
