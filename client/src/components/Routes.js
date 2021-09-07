0b341aef4f3f3ff40d540acf542444505608f88c
austin-rey <austinrey95@gmail.com>
2021-09-07 17:21:19 -0500

    Updated dependencies and API routing

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,33 +1,39 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch,useHistory,Redirect } from 'react-router-dom'
-import AuthState from '../context/authState'
-import Landing from '../pages/Landing'
-import Login from '../pages/Login'
-import Register from '../pages/Register'
-import Home from '../pages/Home'
-import ColorSet from '../pages/ColorSet'
-import Header from './Header'
-import Footer from './Footer'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+  Redirect,
+} from 'react-router-dom';
+import AuthState from '../context/authState';
+import Landing from '../pages/Landing';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Home from '../pages/Home';
+import ColorSet from '../pages/ColorSet';
+import Header from './Header';
+import Footer from './Footer';
 
 const Routes = () => {
-    let history = useHistory();
+  let history = useHistory();
 
-    return (
-        <AuthState>
-            <Router history={history}>
-                <Header history={history}/>
-                <Switch>
-                    <Route exact path='/' component={Landing}/>
-                    <Route exact path='/login' component={Login}/>
-                    <Route exact path='/register' component={Register}/>
-                    <Route exact path='/sets' component={Home}/>
-                    <Route exact path='/set/:id' component={ColorSet}/>
-                    <Redirect to='/' />
-                </Switch>
-                <Footer/>
-            </Router>
-        </AuthState>
-    )
-}
+  return (
+    <AuthState>
+      <Router history={history}>
+        <Header history={history} />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/sets" component={Home} />
+          <Route exact path="/set/:id" component={ColorSet} />
+          <Redirect to="/" />
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthState>
+  );
+};
 
-export default Routes
+export default Routes;
